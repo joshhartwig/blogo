b48d59888f910fc42367cf0fa0189e6515dd8c56
Josh Hartwig <joshman211@gmail.com>
2026-04-29 21:24:26 -0500

    implement template caching and add NewCache function for HTML templates

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,15 +1,19 @@
 package server
 
 import (
+	"html/template"
+	"io/fs"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
-	"text/template"
 
 	"github.com/joshhartwig/blogo/internal/config"
 	"github.com/joshhartwig/blogo/internal/markdown"
 	"github.com/joshhartwig/blogo/internal/models"
 	"github.com/joshhartwig/blogo/internal/posts"
+	"github.com/joshhartwig/blogo/internal/templates"
+	"github.com/joshhartwig/blogo/ui"
 )
 
 type Server struct {
@@ -22,15 +26,35 @@ type Server struct {
 }
 
 func New(siteConfig config.SiteConfig) *Server {
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		log.Fatalf("error building template cache: %v", err)
+	}
+
 	markdownCache := markdown.NewCache(os.DirFS(siteConfig.ContentDir))
 
 	s := &Server{
 		cfg:           siteConfig,
 		mux:           http.NewServeMux(),
 		markdownCache: markdownCache,
+		templateCache: templateCache,
 	}
 
 	s.Routes()
 
 	return s
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	templateFS, err := fs.Sub(ui.Files, "templates")
+	if err != nil {
+		return nil, err
+	}
+	return templates.NewCache(
+		templateFS,
+		"pages/*.html",
+		"partials/*.html",
+		"base.html",
+	)
+
 }
